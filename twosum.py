d9c871550d5d6d653a46aec6391df223828fec23
Marelin <marelinsanorita96@gmail.com>
2020-11-14 23:42:57 +0530

    Leetcode Twosum Implementation

---
+++ twosum.py
@@ -0,0 +1,24 @@
+arr1 = [1,3,5,6]
+target1 = 9
+arr2 = [4,7,2,6]
+target2 = 12
+
+def twosum(arr, target):
+
+    values = dict()
+
+    for i, elem in enumerate(arr):
+        comp = target - elem 
+
+        if comp in values:
+           return [values[comp],i]
+
+        values [elem] = i
+
+    return []
+list1 = twosum(arr1, target1)
+print(list1)
+
+list2 = twosum(arr2, target2)
+print(list2)
+
