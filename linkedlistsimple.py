335a47f94538c791c43b125861d863d364678ce6
Marelin <marelinsanorita96@gmail.com>
2020-11-18 00:24:50 +0530

    This is a simple linkedlist code

---
+++ linkedlistsimple.py
@@ -0,0 +1,22 @@
+class LinkedlistNode:
+    def __init__(self, value, nextNode = None):
+        self.value = value
+        self.nextNode = nextNode
+
+#"5"->"9"->"10"->"4"
+
+node1 = LinkedlistNode("5")
+node2 = LinkedlistNode("9")
+node3 = LinkedlistNode("10")
+node4 = LinkedlistNode("4")
+node1.nextNode = node2
+node2.nextNode = node3
+node3.nextNode = node4
+
+currentNode = node1
+while True:
+    print( currentNode.value,"->")
+    if currentNode.nextNode is None :
+
+        break
+    currentNode = currentNode.nextNode
